01db61e9a0eee7682fdaa91385bdb8fd2eb2a438
Vizyon <Vizyon@MASA-44>
2022-11-05 21:03:43 +0300

    Proje dosyası ekle.

---
+++ Steam Login Manager/Form1.Designer.cs
@@ -0,0 +1,233 @@
+﻿namespace Steam_Login_Manager
+{
+	
+	public partial class Form1 : global::System.Windows.Forms.Form
+	{
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && this.components != null)
+			{
+				this.components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+		private void InitializeComponent()
+		{
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.White;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button1.Location = new System.Drawing.Point(103, 322);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(179, 33);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Login";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(19, 91);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(76, 18);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Username:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Transparent;
+            this.label2.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.label2.Location = new System.Drawing.Point(24, 123);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(71, 18);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Password:";
+            // 
+            // textBox1
+            // 
+            this.textBox1.BackColor = System.Drawing.Color.White;
+            this.textBox1.Location = new System.Drawing.Point(103, 88);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(179, 26);
+            this.textBox1.TabIndex = 3;
+            // 
+            // textBox2
+            // 
+            this.textBox2.BackColor = System.Drawing.Color.White;
+            this.textBox2.Location = new System.Drawing.Point(103, 120);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(179, 26);
+            this.textBox2.TabIndex = 4;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.Transparent;
+            this.label3.ForeColor = System.Drawing.Color.White;
+            this.label3.Location = new System.Drawing.Point(133, 55);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(118, 18);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Add New Account";
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.White;
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button2.Location = new System.Drawing.Point(103, 163);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(179, 34);
+            this.button2.TabIndex = 6;
+            this.button2.Text = "Add Account";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.BackColor = System.Drawing.Color.Transparent;
+            this.label4.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.label4.Location = new System.Drawing.Point(3, 283);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(103, 18);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Select Account:";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(103, 280);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(179, 26);
+            this.comboBox1.TabIndex = 8;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.BackColor = System.Drawing.Color.Transparent;
+            this.label5.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.label5.Location = new System.Drawing.Point(147, 250);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(104, 18);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "Saved Accounts";
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(205)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.button3.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button3.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(220)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.button3.Location = new System.Drawing.Point(338, 2);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(23, 23);
+            this.button3.TabIndex = 30;
+            this.button3.Text = "X";
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.label6.Location = new System.Drawing.Point(-17, 213);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(443, 18);
+            this.label6.TabIndex = 31;
+            this.label6.Text = "---------------------------------------------------------------------------------" +
+    "------\r\n";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Calibri", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label7.ForeColor = System.Drawing.Color.White;
+            this.label7.Location = new System.Drawing.Point(62, 9);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(141, 18);
+            this.label7.TabIndex = 32;
+            this.label7.Text = "Steam Login Manager";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackgroundImage = global::Steam_Login_Manager.Properties.Resources.steam;
+            this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pictureBox1.Location = new System.Drawing.Point(6, 9);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(50, 46);
+            this.pictureBox1.TabIndex = 33;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 18F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(15)))), ((int)(((byte)(15)))), ((int)(((byte)(15)))));
+            this.ClientSize = new System.Drawing.Size(363, 391);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Font = new System.Drawing.Font("Calibri", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ForeColor = System.Drawing.Color.Black;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Steam Login Manager";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+		}
+		private global::System.ComponentModel.IContainer components;
+		private global::System.Windows.Forms.Button button1;
+		private global::System.Windows.Forms.Label label1;
+		private global::System.Windows.Forms.Label label2;
+		private global::System.Windows.Forms.TextBox textBox1;
+		private global::System.Windows.Forms.TextBox textBox2;
+		private global::System.Windows.Forms.Label label3;
+		private global::System.Windows.Forms.Button button2;
+		private global::System.Windows.Forms.Label label4;
+		private global::System.Windows.Forms.ComboBox comboBox1;
+		private global::System.Windows.Forms.Label label5;
+        private global::System.Windows.Forms.Button button3;
+		private global::System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
---
+++ Steam Login Manager/Form1.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Threading;
+using System.Windows.Forms;
+using Microsoft.Win32;
+
+namespace Steam_Login_Manager
+{
+	public partial class Form1 : Form
+	{
+		public Form1()
+		{
+			this.InitializeComponent();
+		}
+
+		private void Form1_Load(object sender, EventArgs e)
+		{
+			new Thread(delegate ()
+			{
+				Thread.Sleep(666);
+				MessageBox.Show("The data you have entered will not be transmitted to us.\nYour data is saved in the steamlogin.txt file in the C disk.", "Steam Login Manager", MessageBoxButtons.OK, MessageBoxIcon.Asterisk);
+			}).Start();
+			Control.CheckForIllegalCrossThreadCalls = false;
+			if (!File.Exists(this.path))
+			{
+				StreamWriter streamWriter = File.CreateText(this.path);
+				streamWriter.Flush();
+				streamWriter.Dispose();
+			}
+			foreach (string text in File.ReadAllLines(this.path).ToList<string>())
+			{
+				string[] array = text.Split(new char[]
+				{
+					','
+				});
+				Form1.user item = new Form1.user(array[0], array[1]);
+				this.comboBox1.Items.Add(array[0]);
+				this.userlist.Add(item);
+			}
+		}
+
+		private void button2_Click(object sender, EventArgs e)
+		{
+			try
+			{
+				List<string> list = File.ReadAllLines(this.path).ToList<string>();
+				Form1.user item = new Form1.user(this.textBox1.Text, this.textBox2.Text);
+				this.userlist.Add(item);
+				list.Add(item.username + "," + item.password);
+				File.WriteAllLines(this.path, list);
+				MessageBox.Show("User Saved Succesfully!", "Steam Login Manager", MessageBoxButtons.OK, MessageBoxIcon.Asterisk);
+			}
+			catch
+			{
+				MessageBox.Show("An error has occurred, data cannot be written if the txt file is open.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Hand);
+			}
+			this.comboBox1.Items.Clear();
+			foreach (Form1.user user in this.userlist)
+			{
+				this.comboBox1.Items.Add(user.username);
+			}
+		}
+
+		private void button1_Click(object sender, EventArgs e)
+		{
+			Process[] processesByName = Process.GetProcessesByName("steam");
+			for (int i = 0; i < processesByName.Length; i++)
+			{
+				processesByName[i].Kill();
+			}
+			processesByName = Process.GetProcessesByName("csgo");
+			for (int i = 0; i < processesByName.Length; i++)
+			{
+				processesByName[i].Kill();
+			}
+			Process.Start(new ProcessStartInfo
+			{
+				FileName = (string)Registry.GetValue("HKEY_CURRENT_USER\\SOFTWARE\\Valve\\Steam", "Steamexe", "null"),
+				Arguments = " -login " + this.userlist[this.comboBox1.SelectedIndex].username + " " + this.userlist[this.comboBox1.SelectedIndex].password
+			});
+		}
+
+		private void button3_Click(object sender, EventArgs e)
+		{
+			base.Hide();
+		}
+
+		private string path = "C:\\steamlogin.txt";
+
+		private List<Form1.user> userlist = new List<Form1.user>();
+
+		private struct user
+		{
+			public user(string user, string pass)
+			{
+				this.username = user;
+				this.password = pass;
+			}
+
+			public string username { get; }
+
+			public string password { get; }
+		}
+
+    }
+}
